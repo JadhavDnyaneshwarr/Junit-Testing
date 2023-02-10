f7b2f687d7baae10297868c18fb6a11c43112c3b
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-10 19:19:20 +0530

    Added: Testing the First Name of User Registration

--- src/test/java/com/bridgelabz/UserRegTest.java
+++ src/test/java/com/bridgelabz/UserRegTest.java
@@ -4,5 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserRegTest {
+    static UserReg userReg = new UserReg();
 
+    @Test
+    public void whenGivenValidFirstNameShouldReturnTrue(){
+        boolean validName = userReg.validateFirstName("Dnyaneshwar");
+        Assertions.assertEquals(true, validName);
+    }
 }
